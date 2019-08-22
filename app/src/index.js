f71df3c87a25d1142e18cf3f0579eeaecac0094e
george <gkephart@cnm.edu>
2019-08-22 10:52:58 -0600

    added the dev server

---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+	return (
+		<>
+			<p> ffjfjfj</p>
+			<h1 className="text-info">hello world</h1>
+		</>
+	)
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
